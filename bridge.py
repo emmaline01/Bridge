e09f2fb1ee9a834ad05dfa4a658e944de37a4e65
emmaline01 <emmaline.mai@gmail.com>
2019-11-02 23:21:34 -0400

    functional buttons

--- bridge.py
+++ bridge.py
@@ -25,8 +25,8 @@ class Player(object):
         allSeats = {'N': ['North', 1], 'E':['East', 2], 'S':['South',3],'W':['West',4]}
         self.seatName = allSeats[seat][0]
         self.playerNum = allSeats[seat][1]
-        self.hand = game.deck[13 * (self.playerNum-1), 13 * self.playerNum]
-        self.partner = game.allPlayers[(self.playerNum + 2) % 4]
+        self.hand = None #game.deck[13 * (self.playerNum-1), 13 * self.playerNum]
+        self.partner = None #game.allPlayers[(self.playerNum + 2) % 4]
 
     def __hash__(self):
         hashable = (self.seat)
@@ -67,12 +67,29 @@ class AI(Player):
         elif len(game.bidSequence) < 1:
             pass
 
+class Button(object):
+    def __init__ (self, tableScreenHeight, position, image):
+        self.image = image
+        topLeft, bottomRight = position
+        self.x0, self.y0 = topLeft
+        self.x, self.y = bottomRight
+        self.y0 += tableScreenHeight
+        self.y += tableScreenHeight
+        self._rect = pygame.Rect(position)
+    
+    def draw(self, screen):
+        screen.blit(self.image, self._rect)
+
+    def event_handler(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN: # is some button clicked
+            if event.button == 1: # is left button clicked
+                eventX, eventY = event.pos
+                if (eventX > self.x0 and eventX < self.x 
+                    and eventY < self.y and eventY > self.y0):
+                    print("clicked!")
+
 #edited from http://blog.lukasperaza.com/getting-started-with-pygame/
 class PygameGame(object):
-
-    """
-    a bunch of stuff is left out of this file, but you can check it out in the Github repo
-    """
 
     def isKeyPressed(self, key):
         ''' return whether a specific key is being held '''
@@ -101,7 +118,30 @@ class PygameGame(object):
         self.handScreen = pygame.Surface(
             (self.width, self.height - self.tableScreenHeight - self.biddingBarScreenHeight - self.biddingOptScreenHeight))
 
+        self.initButtons()
+
         pygame.init()
+
+    def initButtons(self):
+
+        buttonNames = [0, 141, 282, 423, 564]
+        self.buttons = []
+        newRow = []
+        for i in range(5):
+            newPos = ((i*80 + 10, 0), ((i+1)*80, 50))
+            newImg = pygame.image.load(f'imgs/button{buttonNames[i]}.jpeg')
+            newButton = Button(self.tableScreenHeight, newPos, pygame.transform.scale(newImg, (60, 40)))
+            newRow.append(newButton)   
+        self.buttons.append(newRow)
+
+        for row in range(2, 5):
+            newRow = []
+            for col in range(5):
+                newPos = ((col*80 + 10, 50*(row-1)), ((col+1)*80 + 10, 50*row))
+                newImg = pygame.image.load(f'imgs/row{row}button{buttonNames[col]}.jpeg')
+                newButton = Button(self.tableScreenHeight, newPos, pygame.transform.scale(newImg, (60, 40)))
+                newRow.append(newButton)  
+            self.buttons.append(newRow)
 
     def timerFired(self, dt):
         pass
@@ -127,10 +167,19 @@ class PygameGame(object):
                 if event.type == pygame.QUIT:
                     playing = False
 
+                for row in range(len(self.buttons)):
+                    for col in range(len(self.buttons[0])):
+                        self.buttons[row][col].event_handler(event)
+
             screen.fill((70, 130, 50))
             self.biddingOptScreen.fill((50, 110, 30))
             self.biddingBarScreen.fill((0, 0, 0))
             self.handScreen.fill((255, 255, 255))
+
+            for row in range(len(self.buttons)):
+                for col in range(len(self.buttons[0])):
+                    self.buttons[row][col].draw(self.biddingOptScreen)
+            
             screen.blit(self.biddingOptScreen, (0, self.tableScreenHeight))
             screen.blit(self.biddingBarScreen, 
                 (0, self.tableScreenHeight + self.biddingOptScreenHeight))
