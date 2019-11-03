2ef71980bebe611a62924aabb7669810d1b4607b
Yitao Chen <yitaochen@Yitaos-Air-5.wv.cc.cmu.edu>
2019-11-03 15:08:03 -0500

    network and server are socket stuff

--- bridge.py
+++ bridge.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import socket
+from network import Network
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -186,16 +187,20 @@ class PygameGame(object):
         self.net = Network()
 
         self.deck = Deck()
-        self.player1 = Player(self, 'S')
+        self.player = Player(self, 'S')
         self.player2 = Player(self, 'N')
         self.allPlayers = []
         for AISeat in ['E','W']:
             self.allPlayers.append(Player(self,AISeat))
         self.allPlayers.insert(0,self.player2)
-        self.allPlayers.insert(2,self.player1)
+        self.allPlayers.insert(2,self.player)
         for player in self.allPlayers:
             player.setPartner(self)
         self.bidSequence = []
+
+        #does this work
+        print(f'sending,{self.parse_data(self.send_data())}')
+        self.player2.bids.append(self.parse_data(self.send_data()))
 
         self.tableScreenHeight = 200
         self.biddingOptScreenHeight = 200
---
+++ server.py
@@ -0,0 +1,73 @@
+import socket
+from _thread import *
+import sys
+import run
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server = 'localhost'
+port = 5555
+
+server_ip = socket.gethostbyname(server)
+
+try:
+    s.bind((server, port))
+
+except socket.error as e:
+    print(str(e))
+
+s.listen(2)
+print("Waiting for a connection")
+
+currentId = "0"
+
+#### initializeing parameters
+bid = ["0:0,0", "1:0,0"] # initial bids
+##########################
+
+def threaded_client(conn):
+    global currentId, bid
+    conn.send(str.encode(currentId))
+    currentId = "1"
+    reply = ''
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode('utf-8')
+            if not data:
+                conn.send(str.encode("Goodbye"))
+                break
+            else:
+                #### reply is the decoded msg you received
+                print("Recieved: " + reply)
+                arr = reply.split(":")
+                id = int(arr[0])
+
+                #### pos is what is being passed around ## ie the info
+                bid[id] = reply
+
+                if id == 0: nid = 1
+                if id == 1: nid = 0
+
+                #### you definitely have to change pos
+                reply = bid[nid][:]
+                ###### 
+
+                print("Sending: " + reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+    print("Connection Closed")
+    conn.close()
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
+
+    start_new_thread(threaded_client, (conn,))
+
+'''
+each client run its own game and update its para according 
+to the bid that has been passed ?
+'''
