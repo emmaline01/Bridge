60d2faf51778572bcd33258b807e6df645aad965
Yitao Chen <yitaochen@Yitaos-Air-5.wv.cc.cmu.edu>
2019-11-02 21:16:15 -0400

    with players and edits in init

--- bridge.py
+++ bridge.py
@@ -17,13 +17,55 @@ class Deck(object):
             for rank in ranks:
                 self.deck += [Card(suit, rank)]
 
-class Hand(object):
-    def __init__(self, deckObj):
-        self.hand = []
-        for i in range(13):
-            newCard = deckObj.deck[random.randint(0, len(deckObj.deck) - 1 )]
-            self.hand += [newCard]
-            deckObj.deck.remove(newCard)
+class Player(object):
+    def __init__(self, game, seat): 
+        # game is the app object
+        # seat is supposed to be N,S,E,W in str
+        self.seat = seat
+        allSeats = {'N': ['North', 1], 'E':['East', 2], 'S':['South',3],'W':['West',4]}
+        self.seatName = allSeats[seat][0]
+        self.playerNum = allSeats[seat][1]
+        self.hand = game.deck[13 * (self.playerNum-1), 13 * self.playerNum]
+        self.partner = game.allPlayers[(self.playerNum + 2) % 4]
+
+    def __hash__(self):
+        hashable = (self.seat)
+        return hash(hashable)
+
+    def __eq__(self, other):
+        return isinstance(self,player) and self.seat == other.seat
+        
+class RealPlayer(Player):
+    def makeBid(self, game, bid): # bid is from user input
+        game.bidSequence.append(bid)
+
+
+class AI(Player):
+    def __init__(self,game,seat):
+        super().__init__(self,game,seat)
+        self.hcp = 0
+        for card in self.hand:
+            if card.rank == 'A': self.hcp += 4
+            elif card.rank == 'K': self.hcp += 3
+            elif card.rank == 'Q': self.hcp += 2
+            elif card.rank == 'J': self.hcp += 1
+        self.countHand()
+        
+    def countHand(self):
+        self.distribution = {'S':0, 'H':0, 'D':0, 'C':0}
+        for card in self.hand:
+            self.distribution[card.suit] += 1
+            return self.distribution
+
+
+    def makeBid(self, game, turn): # turn should be some count % 4
+        # mute opponent first
+        if self.partner != game.player: return 'Pass'
+
+        if self.hcp < 6:
+            return 'Pass'
+        elif len(game.bidSequence) < 1:
+            pass
 
 class PygameGame(object):
 
@@ -42,7 +84,12 @@ class PygameGame(object):
         self.title = title
 
         self.deck = Deck()
-        self.playerHand = Hand(self.deck)
+        self.player = Player(self, 'S')
+        self.allPlayers = []
+        for AISeat in ['N','E','W']:
+            self.allPlayers.append(Player(self,AISeat))
+        self.allPlayers.insert(2,self.player)
+        self.bidSequence = []
 
         pygame.init()
 
